d8c397072db0cc47125924600731803dccece730
min-shuan <alina092826@gmail.com>
2021-06-08 20:38:33 +0800

    first commit

---
+++ copy.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+
+int main(int argc,char **argv){
+  int fd_in,fd_out;
+  int readsize;
+  char buf[1024];
+
+  //開檔
+  fd_in=open(argc[1],O_RDONLY);
+  fd_out=open(argv[2],O_WRONLY|O_CREAT|O_TRUNC,00600);
+
+  //檢查開檔
+  if(fd_in == -1){
+    printf("Open input file failed\n");
+    return 0;
+  }
+  if(fd_out == -1){
+    printf("Open input file failed\n");
+    return 0;
+  }
+  while(1){
+    //讀檔
+    readsize=read(fd_in,buf,1024);
+    if(readsize<=0){
+      break;
+    }
+    write(fd_out,buf,readsize);
+  }
+
+  //關檔
+  close(fd_in);
+  close(fd_out);
+
+}
---
+++ reverse檔案.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+
+int main(int argc,char **argv){
+  int fd;
+  int filesize;
+  struct stat st;
+  fd=open(argv[1],O_RDWR);
+  stat(argv[1],&st);
+  filesize=st.st_size;
+  //lseek(fd,0,SEEK_END);
+  //filesize=tell(fd); juice沒有這個函式
+  for(int i=0;i<filesize/2;i++){
+    char front[2]={0};
+    char back[2]={0};
+    lseek(fd,i,SEEK_SET);
+    read(fd,front,1);
+
+    lseek(fd,(-1)*i-1, SEEK_END);
+    read(fd, back, 1);
+
+    lseek(fd, i, SEEK_SET);
+    read(fd, back, 1);
+
+    lseek(fd, (-1)*i-1, SEEK_END);
+    read(fd, front, 1);
+  }
+  close(fd);
+  return 0;
+}
